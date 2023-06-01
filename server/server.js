a6ad6afb5cf0214a0ac5cbbc3170bf000c5b45d3
Vishrut Agarwalla <vishrutagarwalla19@gmail.com>
2023-06-01 10:27:52 +0530

    Authentication Done

--- server/server.js
+++ server/server.js
@@ -2,29 +2,121 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-require("dotenv/config");
-
-const app = express();
-
-// Import Routes
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const bcrypt = require("bcrypt");
+const session = require("express-session");
+const User = require("./models/User");
 const questionsRoute = require("./routes/questionsRoute");
 const answersRoute = require("./routes/answersRoute");
+require("dotenv/config");
+// --------------------------- END OF ALL IMPORTS ---------------------------------
+const app = express();
 
 // Middlewares
 app.use(bodyParser.json());
 app.use(cors());
-app.use("/Questions", questionsRoute);
-app.use("/Answers", answersRoute);
-
-// Routes
-// app.get("/home", (req, res) => {
-//   res.send("We are on homepage");
-// });
+app.use(
+  session({
+    secret: "secret-key",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Connect To DB
-mongoose.connect(process.env.DATABASE_URL).then(() => {
-  console.log("Connected to DB");
+mongoose
+  .connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error({ message: error });
+  });
+
+// Passport Configuration
+passport.use(
+  new LocalStrategy(
+    { usernameField: "username" },
+    async (username, password, done) => {
+      try {
+        const user = await User.findOne({ username });
+        if (!user) {
+          return done(null, false, { message: "Incorrect username" });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+          return done(null, false, { message: "Incorrect password" });
+        }
+
+        return done(null, user);
+      } catch (error) {
+        return done(error);
+      }
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user._id);
 });
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (error) {
+    done(error);
+  }
+});
+
+// Signup route
+app.post("/signup", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    // Check if the username is already taken
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ message: "Username already exists" });
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new user
+    const newUser = new User({ username, password: hashedPassword });
+    await newUser.save();
+
+    res.json({ message: "Signup successful" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+// Login route
+app.post("/login", passport.authenticate("local"), (req, res) => {
+  res.json({ message: "Login successful" });
+});
+
+// Protected routes
+const isAuthenticated = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.status(401).json({ message: "Unauthorized" });
+};
+
+// Protected Routes for Questions and Answers
+app.use("/Questions", isAuthenticated, questionsRoute);
+app.use("/Answers", isAuthenticated, answersRoute);
 
 // Listening
 const PORT = 4000;
